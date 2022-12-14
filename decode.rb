70bb61441b92db53dabfb371200bb045f57498bb
Emre Karayaman <karayaman.emre@outlook.com>
2022-12-14 15:27:02 +0300

    methods created

---
+++ decode.rb
@@ -0,0 +1,28 @@
+def decode_char(char)
+  codes = {
+    'a' => '.-', 'b' => '-...', 'c' => '-.-.', 'd' => '-..',
+    'e' => '.', 'f' => '..-.', 'g' => '--.', 'h' => '....', 'i' => '..', 'j' => '.---',
+    'k' => '-.-', 'l' => '.-..', 'm' => '--', 'n' => '-.', 'o' => '---', 'p' => '.--.',
+    'q' => '--.-', 'r' => '.-.', 's' => '...', 't' => '-', 'u' => '..-', 'v' => '...-',
+    'w' => '.--', 'x' => '-..-', 'y' => '-.--', 'z' => '--..'
+  }
+  codes.key(char).upcase
+end
+
+def decode_word(word)
+  decoded_word = ''
+  chars = word.split
+  chars.each do |char|
+    decoded_word += decode_char(char)
+  end
+  decoded_word
+end
+
+def decode_message(message)
+  sentence = []
+  words = message.split('   ')
+  words.each do |word|
+    sentence.push(decode_word(word))
+  end
+  sentence.join(' ')
+end
